33a4f4493b22b1cd6b49bf1b2078f667bcf88025
Dani Koza <danikoza@gmail.com>
2021-06-28 22:06:12 +0300

    finished

--- server/app.js
+++ server/app.js
@@ -7,46 +7,67 @@ dotenv.config();
 
 const dbService = require('./dbService');
 
-app.use(cors());
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // create
-app.post('/insert', (req, res) => {
-    console.log(req.body);
+app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+    
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ data: data}))
+    .catch(err => console.log(err));
 });
 
 // read
-app.get('/getAll', (req, res) => {
-    const dataBase = dbService.getDbServiceInstance();
-    const result = dataBase.getAllData();
-    // console.log(result);
-    result
-        .then(data => res.json({ data: data }))
-        .catch(err => console.log(err));
-});
+app.get('/getAll', (request, response) => {
+    const db = dbService.getDbServiceInstance();
 
+    const result = db.getAllData();
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
 
 // update
+app.patch('/update', (request, response) => {
+    const { id, name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.updateNameById(id, name);
+    
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+});
 
 // delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
 
-// handle unknown paths
-app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+    const result = db.deleteRowById(id);
+    
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
 });
 
+app.get('/search/:name', (request, response) => {
+    const { name } = request.params;
+    const db = dbService.getDbServiceInstance();
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-});
+    const result = db.searchByName(name);
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
 
 app.listen(process.env.PORT, () => console.log('app is running'));
